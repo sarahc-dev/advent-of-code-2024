444cab73ffd8040551b8756e59c0f5312a9cddfa
Sarah Clements <sarahclements@hotmail.co.uk>
2024-12-02 13:30:03 +0000

    Refactor

--- src/main/kotlin/Day1.kt
+++ src/main/kotlin/Day1.kt
@@ -4,18 +4,16 @@ import kotlin.math.abs
 
 class Day1(input: String) {
     private val numberList = input.split(Regex("\\s+")).map { it.toInt() }
-    private val partitioned = numberList.withIndex().partition { it.index % 2 == 0 }
+    private val leftList = numberList.filterIndexed { index, _ -> index % 2 == 0 }.sorted()
+    private val rightList = numberList.filterIndexed { index, _ -> index % 2 != 0 }.sorted()
 
     fun part1(): Int {
-        return partitioned.let { (leftList, rightList) ->
-            leftList.map { it.value }.sorted() zip rightList.map { it.value }.sorted()
-        }.sumOf { (a, b) -> abs(a - b) }
+        return (leftList zip rightList).sumOf { (a, b) -> abs(a - b) }
     }
 
     fun part2(): Int {
-        return partitioned.first.map { it.value }
-            .fold(0) { acc, leftNum ->
-                acc + leftNum * partitioned.second.map { it.value }.count { rightNum -> rightNum == leftNum }
+        return leftList.fold(0) { acc, leftNum ->
+                acc + leftNum * rightList.count { rightNum -> rightNum == leftNum }
             }
     }
 }
