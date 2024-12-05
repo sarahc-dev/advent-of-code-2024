2feed577f365a23fda22ca2439e5083e9f001e72
Sarah Clements <sarahclements@hotmail.co.uk>
2024-12-05 17:29:14 +0000

    Add day 4

---
+++ src/main/kotlin/Day4.kt
@@ -0,0 +1,74 @@
+package org.example
+
+class Day4(input: String) {
+    private val lines = input.split("\n")
+
+    fun part1(): Int {
+        var count = 0
+        lines.forEachIndexed { i, line ->
+            line.forEachIndexed { j, char ->
+                if (char == 'X') {
+                    if ("${lines.getOrNull(i)?.getOrNull(j + 1)}${
+                            lines.getOrNull(i)?.getOrNull(j + 2)
+                        }${lines.getOrNull(i)?.getOrNull(j + 3)}" == "MAS"
+                    ) count++
+                    if ("${lines.getOrNull(i)?.getOrNull(j - 1)}${
+                            lines.getOrNull(i)?.getOrNull(j - 2)
+                        }${lines.getOrNull(i)?.getOrNull(j - 3)}" == "MAS"
+                    ) count++
+                    if ("${lines.getOrNull(i + 1)?.getOrNull(j)}${
+                            lines.getOrNull(i + 2)?.getOrNull(j)
+                        }${lines.getOrNull(i + 3)?.getOrNull(j)}" == "MAS"
+                    ) count++
+                    if ("${lines.getOrNull(i - 1)?.getOrNull(j)}${
+                            lines.getOrNull(i - 2)?.getOrNull(j)
+                        }${lines.getOrNull(i - 3)?.getOrNull(j)}" == "MAS"
+                    ) count++
+                    if ("${lines.getOrNull(i + 1)?.getOrNull(j + 1)}${
+                            lines.getOrNull(i + 2)?.getOrNull(j + 2)
+                        }${lines.getOrNull(i + 3)?.getOrNull(j + 3)}" == "MAS"
+                    ) count++
+                    if ("${lines.getOrNull(i - 1)?.getOrNull(j - 1)}${
+                            lines.getOrNull(i - 2)?.getOrNull(j - 2)
+                        }${lines.getOrNull(i - 3)?.getOrNull(j - 3)}" == "MAS"
+                    ) count++
+                    if ("${lines.getOrNull(i - 1)?.getOrNull(j + 1)}${
+                            lines.getOrNull(i - 2)?.getOrNull(j + 2)
+                        }${lines.getOrNull(i - 3)?.getOrNull(j + 3)}" == "MAS"
+                    ) count++
+                    if ("${lines.getOrNull(i + 1)?.getOrNull(j - 1)}${
+                            lines.getOrNull(i + 2)?.getOrNull(j - 2)
+                        }${lines.getOrNull(i + 3)?.getOrNull(j - 3)}" == "MAS"
+                    ) count++
+                }
+            }
+        }
+        return count
+    }
+
+    fun part2(): Int {
+        var count = 0
+        lines.forEachIndexed { i, line ->
+            line.forEachIndexed { j, char ->
+                if (char == 'A') {
+                    if (
+                        ((lines.getOrNull(i - 1)?.getOrNull(j - 1) == 'M' && lines.getOrNull(i + 1)
+                            ?.getOrNull(j + 1) == 'S')
+                                || (lines.getOrNull(i - 1)?.getOrNull(j - 1) == 'S' && lines.getOrNull(i + 1)
+                            ?.getOrNull(j + 1) == 'M')) &&
+                        ((lines.getOrNull(i - 1)?.getOrNull(j + 1) == 'M' && lines.getOrNull(i + 1)
+                            ?.getOrNull(j - 1) == 'S')
+                                || (lines.getOrNull(i - 1)?.getOrNull(j + 1) == 'S' && lines.getOrNull(i + 1)
+                            ?.getOrNull(j - 1) == 'M'))
+                    ) count++
+
+                }
+            }
+        }
+        return count
+    }
+
+}
+
+
+
---
+++ src/test/kotlin/Day4Test.kt
@@ -0,0 +1,40 @@
+import org.example.Day4
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+
+class Day4Test {
+    @Test
+    fun `Part 1 - sample`() {
+        assertEquals(18, Day4(sampleInput).part1())
+    }
+
+    @Test
+    fun `Part 1 - puzzle input`() {
+        assertEquals(2370, Day4(puzzleInput).part1())
+    }
+
+    @Test
+    fun `Part 2 - sample`() {
+        assertEquals(9, Day4(sampleInput).part2())
+    }
+
+    @Test
+    fun `Part 2 - puzzle input`() {
+        assertEquals(1908, Day4(puzzleInput).part2())
+    }
+
+    private val sampleInput = """
+        MMMSXXMASM
+        MSAMXMSMSA
+        AMXSXMAAMM
+        MSAMASMSMX
+        XMASAMXAMM
+        XXAMMXXAMA
+        SMSMSASXSS
+        SAXAMASAAA
+        MAMMMXMMMM
+        MXMXAXMASX
+        """.trimIndent()
+
+    private val puzzleInput = ClassLoader.getSystemResource("Day4Input.txt").readText()
+}
