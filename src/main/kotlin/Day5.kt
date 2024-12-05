88dbdc96e8333411b5a9ca50a6f6e99a9ff27bc3
Sarah Clements <sarahclements@hotmail.co.uk>
2024-12-05 23:41:00 +0000

    Add day 5

---
+++ src/main/kotlin/Day5.kt
@@ -0,0 +1,41 @@
+package org.example
+
+class Day5(val input: String) {
+    private val splitInput = input.split("\n\n")
+    private val rules = splitInput[0].split("\n").map { rule -> rule.split("|").map { it.toInt() }.let { it[0] to it[1] } }
+    private val updates = splitInput[1].split("\n").map { update -> update.split(",").map { it.toInt() } }
+
+    fun part1(): Int {
+        var count = 0
+        for (update in updates) {
+            val filteredRules = rules.filter { it.first in update && it.second in update }
+            if (filteredRules.all { update.indexOf(it.first) < update.indexOf(it.second) }) {
+                count += update[update.size / 2]
+            }
+        }
+        return count
+    }
+
+    fun part2(): Int {
+        var count = 0
+        for (update in updates) {
+            val filteredRules = rules.filter { it.first in update && it.second in update }
+            if (!filteredRules.all { update.indexOf(it.first) < update.indexOf(it.second) }) {
+                val mutable = update.toMutableList()
+                while (!filteredRules.all { mutable.indexOf(it.first) < mutable.indexOf(it.second) }) {
+                    filteredRules.forEach {
+                        val firstIndex = mutable.indexOf(it.first)
+                        val secondIndex = mutable.indexOf(it.second)
+                        if (firstIndex > secondIndex) {
+                            val temp = mutable[firstIndex]
+                            mutable[firstIndex] = mutable[secondIndex]
+                            mutable[secondIndex] = temp
+                        }
+                    }
+                }
+                count += mutable[mutable.size / 2]
+            }
+        }
+        return count
+    }
+}
---
+++ src/test/kotlin/Day5Test.kt
@@ -0,0 +1,58 @@
+import org.example.Day5
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+
+class Day5Test {
+    @Test
+    fun `Part 1 - sample`() {
+        assertEquals(143, Day5(sampleInput).part1())
+    }
+
+    @Test
+    fun `Part 1 - puzzle input`() {
+        assertEquals(5732, Day5(puzzleInput).part1())
+    }
+
+    @Test
+    fun `Part 2 - sample`() {
+        assertEquals(123, Day5(sampleInput).part2())
+    }
+
+    @Test
+    fun `Part 2 - puzzle input`() {
+        assertEquals(4716, Day5(puzzleInput).part2())
+    }
+
+    private val sampleInput = """
+        47|53
+        97|13
+        97|61
+        97|47
+        75|29
+        61|13
+        75|53
+        29|13
+        97|29
+        53|29
+        61|53
+        97|53
+        61|29
+        47|13
+        75|47
+        97|75
+        47|61
+        75|61
+        47|29
+        75|13
+        53|13
+
+        75,47,61,53,29
+        97,61,53,29,13
+        75,29,13
+        75,97,47,61,53
+        61,13,29
+        97,13,75,29,47
+        """.trimIndent()
+
+    private val puzzleInput = ClassLoader.getSystemResource("Day5Input.txt").readText()
+}
