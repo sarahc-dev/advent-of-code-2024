cf136dd7611b63669b7bbc0eb4c1a85848260463
Sarah Clements <sarahclements@hotmail.co.uk>
2024-12-08 15:47:40 +0000

    Add day 7

---
+++ src/main/kotlin/Day7.kt
@@ -0,0 +1,49 @@
+package org.example
+
+class Day7(input: String) {
+    private val equations = input.split("\n").map { line -> line.split(": ") }.map { values ->
+        Pair(values[0].toLong(), values[1].split(" ").map { it.toInt() })
+    }
+
+    fun part1(): Long {
+        var result: Long = 0
+        for (line in equations) {
+            if (isConfiguration(line)) result += line.first
+        }
+        return result
+    }
+
+    fun part2(): Long {
+        val operators = listOf('*', '+', '|')
+        var result: Long = 0
+        for (line in equations) {
+            if (isConfiguration(line, operators)) result += line.first
+        }
+        return result
+    }
+
+    private fun isConfiguration(pair: Pair<Long, List<Int>>, operators: List<Char> = listOf('*', '+')): Boolean {
+        for (combination in generateOperatorCombinations(pair.second.size - 1, operators)) {
+            var result = pair.second[0].toLong()
+            for (x in combination.indices) {
+                when (combination[x]) {
+                    '*' -> result *= pair.second[x + 1]
+                    '+' -> result += pair.second[x + 1]
+                    else -> result = "$result${pair.second[x + 1]}".toLong()
+                }
+            }
+            if (result == pair.first) return true
+        }
+        return false
+    }
+
+    private fun generateOperatorCombinations(size: Int, operators: List<Char>): List<List<Char>> {
+        return operators.flatMap { operator ->
+            if (size == 1) {
+                listOf(listOf(operator))
+            } else {
+                generateOperatorCombinations(size - 1, operators).map { listOf(operator) + it }
+            }
+        }
+    }
+}
---
+++ src/test/kotlin/Day7Test.kt
@@ -0,0 +1,39 @@
+import org.example.Day7
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+
+class Day7Test {
+    @Test
+    fun `Part 1 - sample`() {
+        assertEquals(3749, Day7(sampleInput).part1())
+    }
+
+    @Test
+    fun `Part 1 - puzzle input`() {
+        assertEquals(1582598718861, Day7(puzzleInput).part1())
+    }
+
+    @Test
+    fun `Part 2 - sample`() {
+        assertEquals(11387, Day7(sampleInput).part2())
+    }
+
+    @Test
+    fun `Part 2 - puzzle input`() {
+        assertEquals(165278151522644, Day7(puzzleInput).part2())
+    }
+
+    private val sampleInput = """
+        190: 10 19
+        3267: 81 40 27
+        83: 17 5
+        156: 15 6
+        7290: 6 8 6 15
+        161011: 16 10 13
+        192: 17 8 14
+        21037: 9 7 18 13
+        292: 11 6 16 20
+        """.trimIndent()
+
+    private val puzzleInput = ClassLoader.getSystemResource("Day7Input.txt").readText()
+}
