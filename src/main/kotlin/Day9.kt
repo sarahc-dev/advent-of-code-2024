aac2be380a1dd113046aafab5ed026ba7c5fa2a3
Sarah Clements <sarahclements@hotmail.co.uk>
2024-12-10 22:59:18 +0000

    Add day 9

---
+++ src/main/kotlin/Day9.kt
@@ -0,0 +1,52 @@
+package org.example
+
+class Day9(val input: String) {
+    fun part1(): Long {
+        val list = createList()
+        while (list.contains(".")) {
+            list[list.indexOf(".")] = list[list.lastIndex]
+            list.removeAt(list.lastIndex)
+        }
+
+        var checksum: Long = 0
+        list.forEachIndexed { index, n ->
+            checksum += index * n.toInt()
+        }
+        return checksum
+    }
+
+    fun part2(): Long {
+        val list = createList()
+        for (num in list[list.lastIndex].toInt() downTo 0) {
+            val countNum = list.count { it == num.toString() }
+            val numIndex = list.indexOf(num.toString())
+            val spaceIndex = list.windowed(countNum).indexOf(List(countNum) { "." })
+            if (spaceIndex != -1 && spaceIndex < numIndex) {
+                for (pos in spaceIndex..<spaceIndex + countNum) {
+                    list[pos] = num.toString()
+                }
+                for (pos in numIndex..<numIndex + countNum) {
+                    list[pos] = "."
+                }
+            }
+        }
+
+        var checksum: Long = 0
+        list.forEachIndexed { index, n ->
+            if (n != ".") checksum += index * n.toInt()
+        }
+        return checksum
+    }
+
+    private fun createList(): MutableList<String> {
+        val list = mutableListOf<String>()
+        for ((index, char) in input.withIndex()) {
+            if (index % 2 == 0) {
+                for (num in 1..char.digitToInt()) list.add("${index / 2}")
+            } else {
+                for (num in 1..char.digitToInt()) list.add(".")
+            }
+        }
+        return list
+    }
+}
---
+++ src/test/kotlin/Day9Test.kt
@@ -0,0 +1,29 @@
+import org.example.Day9
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+
+class Day9Test {
+    @Test
+    fun `Part 1 - sample`() {
+        assertEquals(1928, Day9(sampleInput).part1())
+    }
+
+    @Test
+    fun `Part 1 - puzzle input`() {
+        assertEquals(6307275788409, Day9(puzzleInput).part1())
+    }
+
+    @Test
+    fun `Part 2 - sample`() {
+        assertEquals(2858, Day9(sampleInput).part2())
+    }
+
+    @Test
+    fun `Part 2 - puzzle input`() {
+        assertEquals(6327174563252, Day9(puzzleInput).part2())
+    }
+
+    private val sampleInput = "2333133121414131402".trimIndent()
+
+    private val puzzleInput = ClassLoader.getSystemResource("Day9Input.txt").readText()
+}
