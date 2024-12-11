8b61b5005ec7a85582c7a2260e022f87fba0b965
Sarah Clements <sarahclements@hotmail.co.uk>
2024-12-11 14:04:04 +0000

    Add day 10

---
+++ src/main/kotlin/Day10.kt
@@ -0,0 +1,66 @@
+package org.example
+
+class Day10(input: String) {
+    private val map = input.lines()
+
+    fun part1(): Int {
+        var total = 0
+        for ((index, row) in map.withIndex()) {
+            for ((col, num) in row.withIndex()) {
+                if (num == '0') total += getScore(Pair(index, col))
+            }
+        }
+        return total
+    }
+
+    fun part2(): Int {
+        var total = 0
+        for ((index, row) in map.withIndex()) {
+            for ((col, num) in row.withIndex()) {
+                if (num == '0') total += getRating(Pair(index, col))
+            }
+        }
+        return total
+    }
+
+    private fun getScore(start: Pair<Int, Int>): Int {
+        val trail: MutableList<MutableSet<Pair<Int,Int>>> = mutableListOf(mutableSetOf(start))
+        for (num in 0..9) {
+            val set: MutableSet<Pair<Int,Int>> = mutableSetOf()
+            trail[num].forEach { set.addAll(getNext(num, it)) }
+            if (set.isEmpty()) break else trail.add(set)
+        }
+        return if (trail.size == 10) trail[9].size else 0
+    }
+
+    private fun getRating(start: Pair<Int, Int>): Int {
+        val trails = mutableListOf(mutableListOf(start))
+        var num = 0
+        while (num < 10) {
+            val temp = mutableListOf<MutableList<Pair<Int,Int>>>()
+            trails.forEach { trail ->
+                if (trail.size == num + 1) {
+                    val next = getNext(num, trail[trail.lastIndex])
+
+                    for (i in next.indices.reversed()) {
+                        if (i == 0) trail.add(next[i])
+                        else temp.add((trail + next[i]).toMutableList())
+                    }
+                }
+            }
+            trails.addAll(temp)
+            num++
+        }
+        return trails.count { it.size == 10 }
+    }
+
+    private fun getNext(num: Int, pos: Pair<Int,Int>): MutableList<Pair<Int, Int>> {
+        val next = '0' + num + 1
+        val list: MutableList<Pair<Int, Int>> = mutableListOf()
+        if (pos.first - 1 in map.indices && map[pos.first - 1][pos.second] == next) list.add(Pair(pos.first - 1, pos.second))
+        if (pos.first + 1 in map.indices && map[pos.first + 1][pos.second] == next) list.add(Pair(pos.first + 1, pos.second))
+        if (pos.second - 1 in map[0].indices && map[pos.first][pos.second - 1] == next) list.add(Pair(pos.first, pos.second - 1))
+        if (pos.second + 1 in map[0].indices && map[pos.first][pos.second + 1] == next) list.add(Pair(pos.first, pos.second + 1))
+        return list
+    }
+}
---
+++ src/test/kotlin/Day10Test.kt
@@ -0,0 +1,38 @@
+import org.example.Day10
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+
+class Day10Test {
+    @Test
+    fun `Part 1 - sample`() {
+        assertEquals(36, Day10(sampleInput).part1())
+    }
+
+    @Test
+    fun `Part 1 - puzzle input`() {
+        assertEquals(607, Day10(puzzleInput).part1())
+    }
+
+    @Test
+    fun `Part 2 - sample`() {
+        assertEquals(81, Day10(sampleInput).part2())
+    }
+
+    @Test
+    fun `Part 2 - puzzle input`() {
+        assertEquals(1384, Day10(puzzleInput).part2())
+    }
+
+    private val sampleInput = """
+        89010123
+        78121874
+        87430965
+        96549874
+        45678903
+        32019012
+        01329801
+        10456732
+    """.trimIndent()
+
+    private val puzzleInput = ClassLoader.getSystemResource("Day10Input.txt").readText()
+}
