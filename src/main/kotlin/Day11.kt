a6600c04b4da7589b6ef150410cff330f40c2910
Sarah Clements <sarahclements@hotmail.co.uk>
2025-11-06 12:37:34 +0000

    Coming back to these.. day 11

---
+++ src/main/kotlin/Day11.kt
@@ -0,0 +1,57 @@
+package org.example
+
+class Day11(input: String) {
+    private val inputList = input.split(" ")
+
+    fun part1(times: Int): Int {
+        var result = inputList
+        repeat(times) {
+            result = blink(result)
+        }
+        return result.count()
+    }
+
+    fun part2(times: Int): Long {
+        var inputAsCounts = inputList.groupingBy { it }.eachCount().mapValues { it.value.toLong() }
+        repeat(times) {
+            inputAsCounts = blinkWithCounts(inputAsCounts)
+        }
+        return inputAsCounts.values.sum()
+    }
+
+    private fun blink(input: List<String>): List<String> {
+        val list = mutableListOf<String>()
+        input.forEach { num -> list.addAll(rules(num)) }
+        return list
+    }
+
+    private fun blinkWithCounts(input: Map<String, Long>): Map<String, Long> {
+        val result = mutableMapOf<String, Long>()
+        input.forEach {
+            rules(it.key).forEach { stone ->
+                result[stone] = result.getOrDefault(stone, 0) + it.value
+            }
+        }
+        return result
+    }
+
+    private fun rules(num: String): List<String> =
+        when {
+            num == "0" -> listOf("1")
+            num.length % 2 == 0 -> {
+                val index = num.length / 2
+                listOf(num.take(index), num.substring(index).toInt().toString())
+            }
+            else -> listOf((num.toLong() * 2024).toString())
+        }
+
+//    fun part2(): Int {
+//        var total = 0
+//        for ((index, row) in map.withIndex()) {
+//            for ((col, num) in row.withIndex()) {
+//                if (num == '0') total += getRating(Pair(index, col))
+//            }
+//        }
+//        return total
+//    }
+}
---
+++ src/test/kotlin/Day11Test.kt
@@ -0,0 +1,28 @@
+import org.example.Day11
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+
+class Day11Test {
+    @Test
+    fun `Part 1 - sample`() {
+        assertEquals(22, Day11("125 17").part1(6))
+        assertEquals(55312, Day11("125 17").part1(25))
+    }
+
+    @Test
+    fun `Part 1 - puzzle input`() {
+        assertEquals(194557, Day11(puzzleInput).part1(25))
+    }
+
+    @Test
+    fun `Part 2 - sample`() {
+        assertEquals(55312, Day11("125 17").part2(25))
+    }
+
+    @Test
+    fun `Part 2 - puzzle input`() {
+        assertEquals(231532558973909, Day11(puzzleInput).part2(75))
+    }
+
+    private val puzzleInput = ClassLoader.getSystemResource("Day11Input.txt").readText()
+}
